fe03f6d79237bf84fca558b9186160e6fc44c1fa
huiwenhua <huiwenhua@liquidnetwork.com>
2021-06-23 19:15:19 +0800

    to github

--- settings.js
+++ settings.js
@@ -4,11 +4,11 @@ const NODE_ENV = 'production'
 const serverPort = 5233
 
 // mongodb
-const mongoUrl = NODE_ENV == 'development' ? 'mongodb://liquid:n3tw0rk@dds-2ze5fffe69c1c5541874-pub.mongodb.rds.aliyuncs.com:3717,dds-2ze5fffe69c1c5542283-pub.mongodb.rds.aliyuncs.com:3717/activity?replicaSet=mgset-13269097' : 'mongodb://liquid:n3tw0rk@dds-2ze5fffe69c1c5541.mongodb.rds.aliyuncs.com:3717,dds-2ze5fffe69c1c5542.mongodb.rds.aliyuncs.com:3717/activity?replicaSet=mgset-13269097';
+const mongoUrl = NODE_ENV == 'development' ? 'xxx' : 'xxx';
 
 // redis
 let redisConfig = {
-    host: NODE_ENV == 'development' ? '127.0.0.1' : 'r-2zefe4ff53318e04.redis.rds.aliyuncs.com',
+    host: NODE_ENV == 'development' ? '127.0.0.1' : 'xxx',
     port: 6379,
     db: 1
 }
